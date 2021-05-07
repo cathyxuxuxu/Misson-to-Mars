aa581608ebaee12bd930d03eaaf7362697b35701
Xu tian <xutian@Xus-iMac.lan>
2021-05-07 13:35:11 -0500

    upload module challenge.

--- scraping.py
+++ scraping.py
@@ -1,8 +1,11 @@
+# Import Splinter, BeautifulSoup, and Pandas
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 import datetime as dt
+
+
 
 def scrape_all():
     # Initiate headless driver for deployment   
@@ -16,7 +19,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "img_title_url": url_title(browser)
     }
     # Stop webdriver and return data
     browser.quit()
@@ -88,4 +92,36 @@ def mars_facts():
     df.set_index('description',inplace=True)
 
     return df.to_html()
+
+def url_title(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    for i in range(3,7):
+        hemispheres={}
+        full_image_elem=browser.find_by_tag('img')[i]
+        full_image_elem.click()
+        html = browser.html
+        img_soup = soup(html, 'html.parser')
+        try:
+            url_rel='https://marshemispheres.com/'+img_soup.find('div',class_='downloads').find('li').find('a').get('href')
+            title=img_soup.find('h2',class_='title').text
+        except AttributeError:
+            return None
+    
+        hemispheres['img_url']=url_rel
+        hemispheres['title']=title
+        hemisphere_image_urls.append(hemispheres)
+        browser.back()
+
+    # 4. Print the list that holds the dictionary of each image url and title.
+    return hemisphere_image_urls
+
+
 
